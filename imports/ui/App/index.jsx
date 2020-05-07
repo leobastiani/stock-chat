56a454237f530ebadccc2eaf59e5fd8488f328f4
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2020-05-07 12:06:11 -0300

    Added Login button

--- imports/ui/App/index.jsx
+++ imports/ui/App/index.jsx
@@ -3,8 +3,8 @@ import './style.css';
 import RoomChanger from '/imports/ui/RoomChanger';
 import MessageReader from '/imports/ui/MessageReader';
 import FormMessage from '/imports/ui/FormMessage';
-import { Messages } from '/imports/api/messages';
 import Scroller from '../RoomChanger/Scroller';
+import AccountsUIWrapper from '/imports/ui/AccountsUIWrapper'
 
 const onSend = (room, message) => {
   Meteor.call('messages.insert', room, 'Leo', message)
@@ -14,7 +14,10 @@ export const App = () => {
   const [room, setRoom] = useState('test')
 
   return <div id="container">
-    <div><RoomChanger onChange={setRoom} /></div>
+    <div>
+      <RoomChanger onChange={setRoom} />
+      <AccountsUIWrapper />
+    </div>
     <Scroller>
       { scrollToBottom => <MessageReader scrollToBottom={scrollToBottom} room={room} /> }
     </Scroller>
