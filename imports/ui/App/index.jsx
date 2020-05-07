1125d715f5e482edf85224c2450a5025f2a883c4
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2020-05-07 20:36:51 -0300

    Added header

--- imports/ui/App/index.jsx
+++ imports/ui/App/index.jsx
@@ -24,6 +24,7 @@ export const App = () => {
     <div id='container'>
       <div id='header'>
         <center>
+          <div> Stock Chat | <a href="https://github.com/leobastiani/stock-chat">GitHub</a></div>
           <input
             id='room'
             placeholder='Main room'
