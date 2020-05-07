00d1466353bc6279eb1b4af8578d6a908d699696
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2020-05-07 11:00:33 -0300

    Added MessageCommand test

---
+++ imports/api/MessageCommand.test.js
@@ -0,0 +1,13 @@
+import assert from "assert";
+
+it('can not be a command', () => {
+    const messageCommand = new MessageCommand('hello, how are you?')
+    assert(!messageCommand.isCommand)
+})
+
+it('has properties', () => {
+    const messageCommand = new MessageCommand('/some_command      =   some parameters')
+    assert(messageCommand.isCommand)
+    assert.equal(messageCommand.command, 'some_command')
+    assert.equal(messageCommand.parameters, 'some parameters')
+})
